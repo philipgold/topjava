2bd7e2ea66717fa73568a3e04e4b5c0958183ec7
Philip Goldman <philip.goldm@gmail.com>
2017-11-19 08:44:14 +0200

    Completed 2 task. Updated initDB.sql and populateDB.sql

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -21,5 +22,17 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  date_time   TIMESTAMP,
+  description VARCHAR,
+  calories    INTEGER,
+  user_id     INTEGER NOT NULL,
+  CONSTRAINT user_meals_idx UNIQUE (user_id, id),
+  CONSTRAINT user_date_time_meals_idx UNIQUE (user_id, date_time),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );

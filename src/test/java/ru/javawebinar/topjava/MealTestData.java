97715225a04c6c63e5df3e6ca8129ed0564398f2
Philip Goldman <philip.goldm@gmail.com>
2017-11-20 21:42:01 +0200

    Fixed. Modified assertThat in MealTestData.java. And moved sprig test file to test package

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -1,10 +1,8 @@
 package ru.javawebinar.topjava;
 
-import org.assertj.core.api.SoftAssertions;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.DateTimeUtil;
 
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
@@ -18,12 +16,7 @@ public class MealTestData {
     public static final Meal ADMIN_MEAL_DINNER = new Meal(ADMIN_MEAL_DINNER_ID, DateTimeUtil.parseLocalDateTime("2015-06-01 21:00:00"), "Админ ужин", 1500);
 
     public static void assertMatch(Meal actual, Meal expected) {
-        SoftAssertions softly = new SoftAssertions();
-        softly.assertThat(actual.getId()).isEqualTo(expected.getId());
-        softly.assertThat(actual.getCalories()).isEqualTo(expected.getCalories());
-        softly.assertThat(actual.getDescription()).isEqualTo(expected.getDescription());
-        softly.assertThat(actual.getDateTime()).isEqualTo(expected.getDateTime());
-        softly.assertAll();
+        assertThat(actual).isEqualToComparingFieldByField(expected);
     }
 
     public static void assertMatch(Iterable<Meal> actual, Meal... expected) {

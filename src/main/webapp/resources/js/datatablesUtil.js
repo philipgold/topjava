20bd80db90691ae721c9aac41430930d945dbaac
Philip Goldman <philip.goldm@gmail.com>
2018-01-10 12:00:52 +0200

    Applied 9_08_js_i18n.patch

--- src/main/webapp/resources/js/datatablesUtil.js
+++ src/main/webapp/resources/js/datatablesUtil.js
@@ -17,11 +17,13 @@
 }
 
 function add() {
-    $("#detailsForm").find(":input").val("");
+    $("#modalTitle").html(i18n["addTitle"]);
+    form.find(":input").val("");
     $("#editRow").modal();
 }
 
 function updateRow(id) {
+    $("#modalTitle").html(i18n["editTitle"]);
     $.get(ajaxUrl + id, function (data) {
         $.each(data, function (key, value) {
             form.find("input[name='" + key + "']").val(value);
@@ -36,7 +38,7 @@ function deleteRow(id) {
         type: "DELETE"
     }).done(function () {
         updateTable();
-        successNoty("Deleted");
+        successNoty("common.deleted");
     });
 }
 
@@ -55,7 +57,7 @@ function save() {
     }).done(function () {
         $("#editRow").modal("hide");
         updateTable();
-        successNoty("Saved");
+        successNoty("common.saved");
     });
 }
 
@@ -68,10 +70,10 @@ function closeNoty() {
     }
 }
 
-function successNoty(text) {
+function successNoty(key) {
     closeNoty();
     new Noty({
-        text: "<span class='glyphicon glyphicon-ok'></span> &nbsp;" + text,
+        text: "<span class='glyphicon glyphicon-ok'></span> &nbsp;" + i18n[key],
         type: 'success',
         layout: "bottomRight",
         timeout: 1000
@@ -81,7 +83,7 @@ function successNoty(text) {
 function failNoty(jqXHR) {
     closeNoty();
     failedNote = new Noty({
-        text: "<span class='glyphicon glyphicon-exclamation-sign'></span> &nbsp;Error status: " + jqXHR.status + (jqXHR.responseJSON ? "<br>" + jqXHR.responseJSON : ""),
+        text: "<span class='glyphicon glyphicon-exclamation-sign'></span> &nbsp;" + i18n["common.errorStatus"] + ": " + jqXHR.status + (jqXHR.responseJSON ? "<br>" + jqXHR.responseJSON : ""),
         type: "error",
         layout: "bottomRight"
     }).show();

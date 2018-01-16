e66a894c8039806e3e4dada43466613f72677985
Philip Goldman <philip.goldm@gmail.com>
2018-01-16 10:03:35 +0200

    Applied 10_05_persist_validate_group.patch

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -1,6 +1,10 @@
 package ru.javawebinar.topjava;
 
+import javax.validation.groups.Default;
+
 public class View {
+    public interface Persist extends Default {}
+
     public interface JsonREST {}
     public interface JsonUI {}
 
